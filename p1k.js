d4aea531d01146d15b76813cd8e6da7709029ef7
Daniel Lauzon <daniel.lauzon@gmail.com>
2013-03-15 02:15:48 -0400

    start p1k

---
+++ p1k.js
@@ -0,0 +1,10 @@
+// var b = document.body;
+// var c = document.getElementsByTagName('canvas')[0];
+// var a = c.getContext('2d');
+
+c.width=c.height=600;
+a.translate(300,300);
+a.moveTo(0,0);
+a.lineTo(100,100);
+a.stroke();
+
